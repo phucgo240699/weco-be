b2a079e7ffbe9d89af49cef8ee67dbd962648035
Lý Hiền Phúc <lhph@netcompany.com>
2022-09-26 13:47:47 +0700

    change token exp

--- controllers/users/refreshToken.js
+++ controllers/users/refreshToken.js
@@ -27,12 +27,12 @@ const refreshToken = async (req, res) => {
     const accessToken = jwt.sign(
       { userId: user._id },
       process.env.ACCESS_TOKEN_KEY,
-      { expiresIn: "1h" }
+      { expiresIn: "20s" }
     );
     const refreshToken = jwt.sign(
       { userId: user._id },
       process.env.REFRESH_TOKEN_KEY,
-      { expiresIn: "1d" }
+      { expiresIn: "60s" }
     );
     
     const data = {
